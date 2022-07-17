3c78a73ed01122c0b0780c34f380bd3e6e66e2af
郑伟鑫 <3482525266@qq.com>
2022-07-17 15:38:34 +0800

    将所有参数放到了config中方便调参

--- config.py
+++ config.py
@@ -1,8 +1,13 @@
-import os
-import torch
-
+# 这是一个配置文件，可以直接在这里调参
 
 class Config:
+    # 预处理
+    discrete = 10  # 离散化的间隔大小
+    abnormal_dx = 0.02  # x 方向上异常的点的最小值
+    abnormal_dy = 0.01  # y 方向上异常的点的最小值
+    interpolate_min_point = 10  # 满足插值的最少原始点的个数
+    interpolate_min_gap = 30  # 插值时相邻两个插值节点的最小间隔时间(min)
+
     retrain = True
     max_seqlen = 8
     n_layer = 5
